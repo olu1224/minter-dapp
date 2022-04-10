a7192c078acdfde831f442542913e9a54a8a50b6
olu1224 <100040645+olu1224@users.noreply.github.com>
2022-04-10 17:57:57 -0600

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0xb1422d37050aC5f3aa41F9997634040fBA74e938"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
