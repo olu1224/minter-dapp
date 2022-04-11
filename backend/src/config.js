08f107343475130f0368051bee8f64481e4a5e5f
olu1224 <100040645+olu1224@users.noreply.github.com>
2022-04-10 18:46:33 -0600

    updated backend

--- backend/src/config.js
+++ backend/src/config.js
@@ -37,7 +37,7 @@ const format = {
 };
 
 const extraMetadata = {
-  external_url: "https://snacking bunnies.xyz", // Replace with your website or remove this line if you do not have one.
+  external_url: "https://snackingbunnies.xyz", // Replace with your website or remove this line if you do not have one.
 };
 
 // NFTPort Info
